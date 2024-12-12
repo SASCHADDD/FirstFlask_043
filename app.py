339871ad8b3c85cd2d42d5fc452e094bf49e77a0
SASCHADDD <saschadanu23@gmail.com>
2024-12-12 08:30:18 +0700

    memodifikasi

--- app.py
+++ app.py
@@ -2,18 +2,16 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Route for the home page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Route for form submission
 @app.route('/submit', methods=['POST'])
 def submit():
     name = request.form.get('name')
-    if (name == "Sascha"):
-        return 'Hello, Sayangku !'
-    return f"Hello, {name}!"
+    age = request.form.get('age')
+    city = request.form.get('city')
+    return f"Hello, {name}! Umur Anda adalah {age} tahun dan Anda tinggal di {city}."
 
 if __name__ == '__main__':
     app.run(debug=True)
